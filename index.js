b230658c1050c1c9fe64aafed02dc651ee1555f5
Victor kichwen <victor.kichwen@student.moringaschool.com>
2024-09-25 21:11:54 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const divide = function() {
+    return 2000 / 100;
+};
+
+
+const square = (num) => {
+    return num * num;
+};
+
+
+const add = (a, b) => {
+    return a + b;
+};
+
+console.log(divide());            
+console.log(square(5));          
+console.log(add(10, 15));     
